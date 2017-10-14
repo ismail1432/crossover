7da1dcbda37a1963c7a3e98d9cf49af2d557a9cb
smaine M <contact@smaine.me>
2017-10-14 03:30:00 +0200

    testing work in progress

---
+++ tests/AppBundle/Controller/AdminControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: contact@smaine.me
+ * Date: 14/10/2017
+ * Time: 02:33
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+class AdminControllerTest
+{
+
+}
---
+++ tests/AppBundle/Controller/MediaControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: contact@smaine.me
+ * Date: 14/10/2017
+ * Time: 03:01
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+class MediaControllerTest
+{
+
+}
